95e2d5166e5a060434a97d83ef2deac0640b9622
Cosmina <cosminagheorghe47@gmail.com>
2024-01-22 20:24:36 +0200

    private route

--- src/app/components/trains/trains.component.ts
+++ src/app/components/trains/trains.component.ts
@@ -1,7 +1,6 @@
-// trains.component.ts
-
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';  
 
 @Component({
   selector: 'app-trains',
@@ -10,18 +9,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TrainsComponent implements OnInit {
   trainResults: any[] = [];
+  departure: string | null = null;
+  arrival: string | null = null;
+  date: string | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    // Retrieve search results from the route parameters
     this.route.queryParams.subscribe(params => {
       const results = params['results'];
+      this.departure = params['departure'];
+      this.arrival = params['arrival'];
+      this.date = params['date'];
+
       if (results) {
         this.trainResults = JSON.parse(results);
-      } else {
-        // Handle the case when results are not available
       }
+
+      console.log('Departure:', this.departure);
+      console.log('Arrival:', this.arrival);
+      console.log('Date:', this.date);
+
+      console.log('Train Results:', this.trainResults);
     });
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.getCurrentUser() !== null;
+  }
+
+  bookTrain(train: any): void {
+    this.router.navigate(['/bookings']);
   }
 }
